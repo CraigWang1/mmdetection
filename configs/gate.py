eac7710a7ab74e06fc3e439213cb4e1e242be6aa
CraigWang1 <55901251+CraigWang1@users.noreply.github.com>
2020-02-27 18:12:22 -0800

    Update gate.py

--- configs/gate.py
+++ configs/gate.py
@@ -99,7 +99,7 @@ test_cfg = dict(
 )
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '/content/unzipped_dset/nodist_640_COCO/data/COCO'    #######I CHANGED THIS
+data_root = '/content/unzipped_dset/nodist_640_COCO/data/COCO/'    #######I CHANGED THIS
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
